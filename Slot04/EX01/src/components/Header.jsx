da2fb02497dbfabd7f87dfd9ebf68394005e2ae3
maidanghuy <huymaidang.work@gmail.com>
2026-01-13 20:16:12 +0700

    init slot4/ex1: create React project with Vite

---
+++ Slot04/EX01/src/components/Header.jsx
@@ -0,0 +1,47 @@
+// Header.jsx - Navbar component
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/">
+          Lab01 React App
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/blog">
+              Blog
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+              Contact
+            </Nav.Link>
+          </Nav>
+
+          <Nav>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/register">
+              Register
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
---
+++ Slot04/EX01/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import OrchidList from "../components/OrchidList";
+
+function Home() {
+  return (
+    <>
+      <h2 className="mb-4">Orchid Collection</h2>
+      <OrchidList />
+    </>
+  );
+}
+
+export default Home;
